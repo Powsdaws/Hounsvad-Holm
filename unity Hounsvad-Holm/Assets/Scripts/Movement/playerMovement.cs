d1307d6ccd9d8ead2611377a4e6909eff2ce0223
Powsdaws <mariusholm@hotmail.dk>
2023-04-07 16:35:54 +0200

    Movement done

--- unity Hounsvad-Holm/Assets/Scripts/Movement/playerMovement.cs
+++ unity Hounsvad-Holm/Assets/Scripts/Movement/playerMovement.cs
@@ -4,63 +4,48 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float speed = 5f; // The speed at which the player moves
-    public float jumpForce = 10f; // The force with which the player jumps
-    public LayerMask groundLayers; // A layer mask that defines what counts as ground for the player
+    public float speed;
+    public float groundDist;
 
-    private Rigidbody rb; // The player's rigidbody component
-    private Animator animator; // The player's animator component
-    private bool isFacingRight = true; // Whether the player is facing right or left
-    private bool isGrounded = false; // Whether the player is currently grounded or not
+    public LayerMask terrainLayer;
+    public Rigidbody rb;
+    public SpriteRenderer sr;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
     }
 
-    void FixedUpdate()
+    
+
+    void Update()
     {
-        // Check if the player is grounded
-        isGrounded = Physics.Raycast(transform.position, Vector2.down, 0.1f, groundLayers);
+        RaycastHit hit;
+        Vector3 castPos = transform.position;
+        castPos.y += 1;
 
-        // Get the player's horizontal input
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        float verticalInput = Input.GetAxis("Vertical");
-
-        // Move the player horizontally
-        Vector2 movement = new Vector2(horizontalInput * speed, rb.velocity.y);
-        rb.velocity = movement;
-
-        Vector2 movementvertical = new Vector2(verticalInput * speed, rb.velocity.x);
-        rb.velocity = movement;
-
-        // Flip the player's sprite if necessary
-        if (horizontalInput > 0 && !isFacingRight)
+        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer))
         {
-            Flip();
-        }
-        else if (horizontalInput < 0 && isFacingRight)
-        {
-            Flip();
+            if (hit.collider != null)
+            {
+                Vector3 movePos = transform.position;
+                movePos.y = hit.point.y + groundDist;
+                transform.position = movePos;
+            }
         }
 
-        // Play the running animation if the player is moving horizontally
-        animator.SetBool("isRunning", Mathf.Abs(horizontalInput) > 0);
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 moveDir = new Vector3(x, 0, y);
+        rb.velocity = moveDir * speed;
 
-        // Check if the player should jump
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (x != 0 && x < 0)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
-            animator.SetTrigger("jump");
+            sr.flipX = true;
         }
-    }
-
-    void Flip()
-    {
-        // Flip the player's sprite
-        isFacingRight = !isFacingRight;
-        transform.Rotate(0f, 180f, 0f);
+        else if (x != 0 && x > 0)
+        {
+            sr.flipX = false;
+        }
     }
 }
