8c3ec34039be0de53d965af4505907a1fd37470c
Powsdaws <mariusholm@hotmail.dk>
2023-04-12 19:51:55 +0200

    kode ændring

--- unity Hounsvad-Holm/Assets/Scripts/Inventory/inventoryManager.cs
+++ unity Hounsvad-Holm/Assets/Scripts/Inventory/inventoryManager.cs
@@ -10,6 +10,10 @@ public class inventoryManager : MonoBehaviour
     public static inventoryManager Instance;
     public List<Item> Items = new List<Item>();
 
+    //Vis i inventory...
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+
 
     private void Awake()
     {
@@ -29,5 +33,17 @@ public class inventoryManager : MonoBehaviour
     public void Remove(Item item)
     {
         Items.Remove(item);
+    }
+
+
+
+    public void ListItems()
+    {
+        foreach (var item in Items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemName = obj.transform.Find("Item/Itemname").GetComponent<Text>();
+
+        }
     }
 }
